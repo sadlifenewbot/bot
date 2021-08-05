ed25562caba12f4c17c80076a2030779318e8b78
MarkKoz <KozlovMark@gmail.com>
2021-08-05 16:32:44 -0700

    Time: replace discord_timestamp calls with format_relative
    Use the latter where the former was being called with the relative
    format type.

--- bot/exts/info/information.py
+++ bot/exts/info/information.py
@@ -170,7 +170,7 @@ class Information(Cog):
         """Returns an embed full of server information."""
         embed = Embed(colour=Colour.blurple(), title="Server Information")
 
-        created = time.discord_timestamp(ctx.guild.created_at, time.TimestampFormats.RELATIVE)
+        created = time.format_relative(ctx.guild.created_at)
         region = ctx.guild.region
         num_roles = len(ctx.guild.roles) - 1  # Exclude @everyone
 
@@ -240,7 +240,7 @@ class Information(Cog):
         """Creates an embed containing information on the `user`."""
         on_server = bool(ctx.guild.get_member(user.id))
 
-        created = time.discord_timestamp(user.created_at, time.TimestampFormats.RELATIVE)
+        created = time.format_relative(user.created_at)
 
         name = str(user)
         if on_server and user.nick:
@@ -260,7 +260,7 @@ class Information(Cog):
 
         if on_server:
             if user.joined_at:
-                joined = time.discord_timestamp(user.joined_at, time.TimestampFormats.RELATIVE)
+                joined = time.format_relative(user.joined_at)
             else:
                 joined = "Unable to get join date"
             # The 0 is for excluding the default @everyone role,
